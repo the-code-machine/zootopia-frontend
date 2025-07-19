12ec35bc501822a050625bdf75126adeb23be8b4
leenalokhande <131686576+leenalokhande@users.noreply.github.com>
2025-07-19 15:44:27 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         rel="noopener noreferrer"
         className="text-blue-600 underline text-lg"
       >
-        Visit Zootopia Website
+        leena Bhootni hai
       </a>
     </div>
   );
